3a1178e14c141b68b698e1e5cfd4562ec5df759c
LY <liyan2761@qq.com>
2017-03-25 20:15:49 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,12 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+
+
+
+
+
+    
   </div>
 </template>
 
