ffd17d8e57a0ba0789b8178a2f0703353c509ad2
LY <liyan2761@qq.com>
2017-03-26 18:23:08 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,17 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
 <style>
    @import  './styles/reset.less';
 </style>
--- src/views/personalCenter.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>personalCenter</div>
-</template>
