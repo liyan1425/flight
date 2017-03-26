e1ac0f3b1897f4143a8f225e6b93f81ee52894a4
LY <liyan2761@qq.com>
2017-03-26 16:17:28 +0800

    add to router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-
-
-
-
-
-    
+    <header>
+			<nav class="navbar navbar-default">
+			    <div class="container">
+			    	<div class="collapse navbar-collapse ">
+				      <ul class="nav navbar-nav ">
+				    	<li>
+				    		<a href="index.html"><img src="src/assets/gQ4fcp.png" class="img-responsive image" alt="Responsive image"></a>
+				    	</li>	 
+				      </ul>
+					  <ul class="nav navbar-nav right">
+				        <li ><a href="#">首页</a></li>
+				        <li ><a href="html/login.html">登陆</a></li>
+				        <li ><a href="html/register.html">注册</a></li>
+				        <li ><a href="#">个人中心</a></li>
+				      </ul>
+			    	</div>
+			    </div>		 	
+			 </nav>	
+    </header>
+    <router-view class="view"></router-view>
   </div>
 </template>
 
@@ -37,30 +35,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  /* @import  './styles/reset.css';*/
 </style>
